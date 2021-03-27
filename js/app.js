a50324092262a5b07ea10fcc1dbf50b6ad6255c7
computergrrl <anani.mowse@gmail.com>
2021-03-26 23:27:16 -0700

    Messaging alerts and toggle styling adjust

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ for (let link of trafficLinks) {
 }
 
 //create alert message 
-alert.innerHTML = `<p><strong>You have unread messages</strong></p>
+alert.innerHTML = `<p><strong>You have 2 unread messages</strong></p>
 <p class="close">X</p>`;
 
 const closeButton = document.querySelector(".close");
@@ -30,6 +30,8 @@ closeButton.addEventListener("click", (e) => {
   alert.style.display = "none";
 });
 
+
+//Create modal window popup for messages
 const bellDiv = document.querySelector("#bell-div");
 const modal = document.querySelector("#modal");
 
@@ -43,82 +45,88 @@ bellDiv.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-
+//Close modal window
 document.addEventListener("click", (e) => {
   if (e.target.textContent == "X") {
     modal.classList.add("hide");
   }
 });
 
-/********************************************************************Following code function borrowed from https://www.w3schools.com/howto/howto_js_autocomplete.asp 
+/********************************************************************
+                   CODE FOR AUTOCOMPLETE DROPDOWN 
+                Following code functions borrowed from
+          https://www.w3schools.com/howto/howto_js_autocomplete.asp 
  
- ******************************************************************/
+ ***********************************************************************/
 
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-          b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+  inp.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
+          (or any other open lists of autocompleted values:*/
+          closeAllLists();
+        });
+        a.appendChild(b);
       }
+    }
   });
   /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+      increase the currentFocus variable:*/
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) { //up
+      /*If the arrow UP key is pressed,
+      decrease the currentFocus variable:*/
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
       }
+    }
   });
+
   function addActive(x) {
     /*a function to classify an item as "active":*/
     if (!x) return false;
@@ -129,12 +137,14 @@ function autocomplete(inp, arr) {
     /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
+
   function removeActive(x) {
     /*a function to remove the "active" class from all autocomplete items:*/
     for (var i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
+
   function closeAllLists(elmnt) {
     /*close all autocomplete lists in the document,
     except the one passed as an argument:*/
@@ -147,14 +157,55 @@ function autocomplete(inp, arr) {
   }
   /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
+    closeAllLists(e.target);
   });
 }
+/********************************************************************* 
+                END OF AUTOCOMPLETE DROPDOWN CODE          
+***************************************************************** */
 
-/*An array containing all the country names in the world:*/
+
+
+/*An array containing members of Hogwarts community:*/
 const members = [
   "Harry Potter", "Ron Weasley", "Hermione Granger", "Severus Snape (Prof)", "Minerva McGonagall (Prof)", "Luna Lovegood", "Nevell Longbottom", "Draco Malfoy", "Rubeus Hagrid", "Alastor 'Mad-Eye' Moody (Prof)", "Albus Dumbledore (Headmaster)", "Fred Weasley", "George Weasley", "Ginny Weasley", "Percy Weasley", "Lavender Brown", "Parvati Patil", "Cedric Diggory", "Remus Lupin (Prof)", "Pansy Parkinson", "Seamus Finnigan", "Vincent Crabbe", "Gregory Goyle", "Cho Chang", "Roger Davies"
 ];
 
-/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+/*initiate the autocomplete function on the "myInput" element, and pass along the members array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), members);
+
+
+
+/* Code to pop up an alert message if the user tries to send a message with input fields left blank */
+const memberName = document.getElementById("myInput");
+const memberMessage = document.getElementById("myMessage");
+const sendMessage = document.getElementById("sendMessage");
+
+sendMessage.addEventListener("click", () => {
+  if(memberName.value == "") {
+    window.alert("The member name can not be blank. Please enter the name of user you want to send a message to");
+  } else if(memberMessage.value == "") {
+    window.alert(`The message field can not be blank. Please enter the message you want to send to ${memberName.value.toUpperCase()}`);
+  } else {
+    messageSent();
+  }
+});
+
+
+//message sent confirmation and reset input fields
+function messageSent() {
+  window.alert(`Your message has been sent to ${memberName.value}`);
+  memberMessage.value = "";
+  memberName.value = "";
+}
+
+const emailToggle = document.getElementById("email-toggle");
+const profileToggle = document.getElementById("profile-toggle");
+
+emailToggle.addEventListener("click", (e) => {
+  e.target.checked == true ? console.log("it's checked!") : console.log("it's NOOOOOT checked");
+})
+
+profileToggle.addEventListener("click", (e) => {
+  e.target.checked == true ? console.log("it's checked!") : console.log("it's NOOOOOT checked");
+})
